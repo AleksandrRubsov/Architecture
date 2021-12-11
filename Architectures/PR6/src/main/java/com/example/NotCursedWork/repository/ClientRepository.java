c384f108f5b73b17b55b8b8fc373bf2080f51c60
AleksandrRubsov <70627203+AleksandrRubsov@users.noreply.github.com>
2021-12-11 21:58:27 +0300

    Delete Architectures/PR6 directory

--- Architectures/PR6/src/main/java/com/example/NotCursedWork/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.NotCursedWork.repository;
-
-import com.example.NotCursedWork.models.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Long> {
-}
